9613e1b5f0b2907f047a5536c1e56c10a6c0c288
Lorin Hardy <lorin.hardy@gmail.com>
2017-11-17 15:05:38 -0700

    Updated version number.

--- lmh_wp_generic.php
+++ lmh_wp_generic.php
@@ -3,7 +3,7 @@
 Plugin Name: Generic Site functions.
 Description: Site specific code for this website. This file replaces the functions.php file and acts as a way to run functions from within the context of a plugin. Do not delete or disactivate this plugin or it will disable certain functionality on this site.
 Author: LMH Productions
-Version: 0.1
+Version: 1.1
 Author URI: http://lmhproductions.co/
 */
 /* Begin Adding Functions Below This Line; Do not include an opening PHP tag as this sample code already includes one! */
